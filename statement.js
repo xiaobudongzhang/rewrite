3038e0162f97de1f6af59f205e02bc066fd081d4
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-12-22 16:55:24 +0800

    修改作用域

--- statement.js
+++ statement.js
@@ -2,62 +2,7 @@ var fs = require('fs');
 
 var invoice = "";
 var plays = "";
-function totalAmount(){
-    let result = 0;
-    for (let perf of invoice.performances) {
-        totalAmount += amountFor(perf);
-    }
-    return result;
-}
-function totalVolumeCredits(){
-    let result = 0;
-    for (let perf of invoice.performances) {
-        result += volumeCreditsFor(perf);
-    }
-    return result;
-}
-function usd(aNumber){
-    return new Intl.NumberFormat("en-US",{
-        style:"currency",
-        currency:"USD",
-        minimumFractionDigits:2
-    }).format(aNumber/100)
-}
 
-function volumeCreditsFor(aPerformance){
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" == playFor(aPerformance).type) {
-        result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
-}
-
-
-function playFor(aPerformance){
-    return plays[aPerformance.playID]
-}
-function amountFor(aPerformance){
-    let thisAmount = 0;
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-            thithisAmount = 40000;
-            if (aPerformance.audience > 30) {
-                thisAmount += 1000 * (aPerformance.audience -30)
-            }
-            break;
-        case "comedy":
-            thisAmount = 30000;
-            if (aPerformance.audience > 20) {
-                thisAmount += 1000 + 500 * (aPerformance.audience -20)
-            }
-            thisAmount += 300 * aPerformance.audience
-            break;
-            default:
-                throw new Error(`unknown type:${playFor(aPerformance).type}`);
-    }
-    return thisAmount
-}
 function getFileByPath(path){
     return new Promise(function(resolve, reject){
         fs.readFile(path,'utf8',function (err, data) {
@@ -82,7 +27,62 @@ function statement(invoice) {
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
-
+    function totalAmount(){
+        let result = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return result;
+    }
+    function totalVolumeCredits(){
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
+    function usd(aNumber){
+        return new Intl.NumberFormat("en-US",{
+            style:"currency",
+            currency:"USD",
+            minimumFractionDigits:2
+        }).format(aNumber/100)
+    }
+    
+    function volumeCreditsFor(aPerformance){
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" == playFor(aPerformance).type) {
+            result += Math.floor(aPerformance.audience / 5);
+        }
+        return result;
+    }
+    
+    
+    function playFor(aPerformance){
+        return plays[aPerformance.playID]
+    }
+    function amountFor(aPerformance){
+        let thisAmount = 0;
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                thithisAmount = 40000;
+                if (aPerformance.audience > 30) {
+                    thisAmount += 1000 * (aPerformance.audience -30)
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (aPerformance.audience > 20) {
+                    thisAmount += 1000 + 500 * (aPerformance.audience -20)
+                }
+                thisAmount += 300 * aPerformance.audience
+                break;
+                default:
+                    throw new Error(`unknown type:${playFor(aPerformance).type}`);
+        }
+        return thisAmount
+    }
 }
 
 
@@ -94,8 +94,7 @@ getFileByPath('plays.json')
 })
 .then(function(data){ 
     invoice = JSON.parse(data)
-    invoice = invoice[0]
-   console.log(statement(invoice));
+   console.log(statement(invoice[0]));
 })
 .catch(function(err){
     console.log('err:' + err);
