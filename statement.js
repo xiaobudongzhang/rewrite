f8b24984ac7e89500c501a1d8ab9e20ca70d6043
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-12-22 16:21:37 +0800

    改变函数声明

--- statement.js
+++ statement.js
@@ -3,12 +3,12 @@ var fs = require('fs');
 var invoice = "";
 var plays = "";
 
-function format(aNumber){
+function usd(aNumber){
     return new Intl.NumberFormat("en-US",{
         style:"currency",
         currency:"USD",
         minimumFractionDigits:2
-    }).format(aNumber)
+    }).format(aNumber/100)
 }
 
 function volumeCreditsFor(aPerformance){
@@ -67,11 +67,11 @@ function statement(invoice, plays) {
 
         volumeCredits = volumeCreditsFor(perf);
 
-        result += ` ${playFor(perf).name} : ${format(amountFor(perf)/100)}  (${perf.audience} seats)\n`
+        result += ` ${playFor(perf).name} : ${usd(amountFor(perf))}  (${perf.audience} seats)\n`
         totalAmount += amountFor(perf);
     }
 
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
